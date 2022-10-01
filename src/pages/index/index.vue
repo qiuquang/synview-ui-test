df49d067fd8ffebe3dcb8e13fe4cd8a8bbb5fd3a
qiuquan <18306485667@163.com>
2022-10-01 15:08:30 +0800

    提交

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,10 +1,10 @@
 <template>
-	<view class="">
-		<view class="status-bar"></view>
-		<syn-left-capsule></syn-left-capsule>
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
+	<view class="page-index">
+		<syn-status-bar></syn-status-bar>
+		<view class="commonPageHeader">
+			<syn-left-capsule></syn-left-capsule>
+			<syn-title-bar pageTitle="三翼鸟测试项目"></syn-title-bar>
+			<syn-right-capsule-block></syn-right-capsule-block>
 		</view>
 	</view>
 </template>
@@ -13,7 +13,6 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
 			}
 		},
 		onLoad() {
@@ -25,30 +24,6 @@
 	}
 </script>
 
-<style>
-	.status-bar{
-		height: var(--status-bar-height);
-	}
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin: 200rpx auto 50rpx auto;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+<style scped lang="scss">
+	
 </style>
